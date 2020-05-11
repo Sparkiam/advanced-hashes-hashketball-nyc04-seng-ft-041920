be3cfaef14c79af99c6ae7349b115b31a4fe51d7
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-11 04:54:25 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,218 @@ def game_hash
 end
 
 # Write code here
+
+
+# Write your code below game_hash
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        {
+          player_name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        {
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        {
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        {
+          player_name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        {
+          player_name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        {
+          player_name: "Bismack Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 22,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        {
+          player_name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        {
+          player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        {
+          player_name: "Kemba Walker",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 7,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
+  }
+end
+
+# Write code here
+
+
+def num_points_scored(player)
+  game_hash.each do |k, v|
+    v[:players].each do |k|
+    if player == k[:player_name]
+      return k[:points]
+        end
+      end
+        end
+end
+
+def shoe_size(player)
+  game_hash.each do |k, v|
+    v[:players].each do |k|
+    if player == k[:player_name]
+      return k[:shoe]
+        end
+      end
+        end
+end
+
+def team_colors(name)
+  game_hash.each do |k,v|
+    if name == v[:team_name]
+    return v[:colors]
+       end
+      end
+        end
+
+
+def team_names
+  franchise = []
+  game_hash.each do |k,v|
+    franchise << v[:team_name]
+  end
+  return franchise
+end
+
+def player_numbers(team_name)
+  jordan = []
+  game_hash.each do |k,v|
+      if team_name == v[:team_name]
+        v[:players].each do |k|
+        jordan << k[:number]
+        end
+      end
+  end
+  return jordan
+end 
+
+
+
+#Build a method, player_stats, that takes in an argument of a player's name and returns a hash of that player's stats.
+
+def player_stats(name)
+  oscar = {}
+  game_hash.each do |k,v|
+    v[:players].each do |k|
+    if name == k[:player_name]
+      oscar = k
+    end
+    end
+  end
+  return oscar
+end
+
+#Then, return that player's number of rebounds. Remember to think about return values here.
+
+
+
+def big_shoe_rebounds
+  shoe_size = 0
+  rodman = 0
+  game_hash.each do |k,v|
+    v[:players].each do |k|
+      if shoe_size < k[:shoe]
+        shoe_size = k[:shoe]
+        rodman = k[:rebounds]
+      end
+    end
+  end
+  return rodman
+end
